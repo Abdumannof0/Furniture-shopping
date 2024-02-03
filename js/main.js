bb3bbbe30d64af37184b031d92367cb0864d538e
Abduvaliev <140178160+Abdumannof0@users.noreply.github.com>
2024-02-04 00:44:03 +0300

    #fix #scss #js cartlar product id boyicha cartTab panelga tuwmoqda hatolar mavjud emas

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
 const iconCard = document.querySelector('.icon_cart')
 const closeCart = document.querySelector('.close')
 const body = document.querySelector('body')
-const listProductHTML = document.querySelector('.listProduct')
+const listProductHTML = document.querySelector('.list_product')
+const listCartHTML = document.querySelector('.list_cart')
+let iconCartBtn = document.querySelector('.logo_basket span')
 
 let listProducts = [];
+let carts = []
 
 iconCard.addEventListener('click', () => {
     body.classList.toggle('show_cart')
@@ -16,18 +19,75 @@ closeCart.addEventListener('click', () => {
 const addDataToHTML = () => {
     listProductHTML.innerHTML = '';
     if (listProducts.length > 0) {
-        listProducts.forEach(data => {
-            let newProduct = document.createElement('div')
+        listProducts.forEach(product => {
+            const newProduct = document.createElement('div')
             newProduct.classList.add('item')
+            newProduct.dataset.id = product.id
             newProduct.innerHTML =
                 `
-                <img src="./image/1.png" alt="" class="item_img" />
-                    <h2 class="item_title">Furniture</h2>
-                    <h3 class="item_price">100.00</h3>
-                <button class="item_btn">Add to Cart</button>
-
+            <img src="${product.image}" alt="" class="item_img" />
+            <h2 class="item_title">${product.name}</h2>
+            <h3 class="item_price">${product.price}</h3>
+            <button class="item_btn">Add to Cart</button>
             `;
             listProductHTML.appendChild(newProduct)
+        })
+    }
+}
+
+listProductHTML.addEventListener('click', (event) => {
+    const positionClick = event.target;
+    if (positionClick.classList.contains('item_btn')) {
+        const product_id = positionClick.parentElement.dataset.id
+        addToCart(product_id)
+    }
+})
+
+const addToCart = (product_id) => {
+    let positionThisProductInCart = carts
+        .findIndex((value) => value.product_id == product_id);
+    if (carts.length <= 0) {
+        carts = [{
+            product_id: product_id,
+            quantity: 1
+        }]
+    } else if (positionThisProductInCart < 0) {
+        carts.push({
+            product_id: product_id,
+            quantity: 1
+        })
+    } else {
+        carts[positionThisProductInCart]
+            .quantity = carts[positionThisProductInCart]
+                .quantity + 1;
+    }
+    addCartToHTML()
+    console.log(carts);
+}
+
+const addCartToHTML = () => {
+    listCartHTML.innerHTML = '';
+    if (carts.length > 0) {
+        carts.forEach(cart => {
+            let newCart = document.createElement('div')
+            newCart.classList.add('total_item')
+            let positionProduct = listProducts
+                .findIndex((value) => value.id == cart.product_id)
+            let info = listProducts[positionProduct]
+            newCart.innerHTML =
+                `
+            <div class="total_img">
+             <img src="${info.image}" alt="" />
+         </div>
+         <div class="total_name"></div>
+         <div class="total_price">$</div>
+         <div class="quantity">
+          <button class="minus">-</button>
+          <button>1</button>
+          <button class="plus">+</button>
+        </div>
+            `;
+            listCartHTML.appendChild(newCart)
         })
     }
 }
@@ -36,8 +96,7 @@ const initApp = () => {
     fetch('products.json')
         .then(res => res.json())
         .then(data => {
-            listProducts => data;
-            // console.log(data);
+            listProducts = data;
             addDataToHTML()
         })
 }
